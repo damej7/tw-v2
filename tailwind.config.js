55fb5f5fa071dae99c8b41fb02b1d03e079f0350
Dame <gban1mj27@gmail.com>
2025-02-13 13:38:00 +0700

    finishing utility part 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.html"], // Sesuaikan path jika HTML ada di dalam folder lain
+  // content: ["./*.html"], // Sesuaikan path jika HTML ada di dalam folder lain
+  content: ["./src/**/*.html", "./dist/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
-        japan: ["42dot Sans", "serif"]
+        japan: ["42dot Sans", "serif"],
       },
       colors: {
         coffee: '#c0ff33',
